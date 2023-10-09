98273d2b71bf071643538b4e1825c04c3f9b165f
Oliver <95024811+samaradeoliveira@users.noreply.github.com>
2023-10-09 09:11:16 -0300

    Update detect_face_with_mask.py

--- detect_face_with_mask.py
+++ detect_face_with_mask.py
@@ -17,11 +17,12 @@ while True:
 
     
 
-    # 2. Converta a imagem em um array Numpy e aumente a dimensão
-
+    # 2. Converta a imagem em um array Numpy e aumente a dimensão para o funcionamento correto das imagens treinadas
+    test_image = np.array(img, dtype=np.float32)
+    test_image = np.expand_dims(test_image, axis=0)
     
-    # 3. Normalize a imagem
-   
+    # 3. Normalize a imagem (ajuda no treinamento eficaz da máquina, especialmente em redes neurais) (rapidez no processamento de imagens)
+    normalised_image = test_image/255.0
 
     # Preveja o resultado
    
