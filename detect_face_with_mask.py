e6af0d6849d4c180f52133a71a48ae4aee1a3041
Oliver <95024811+samaradeoliveira@users.noreply.github.com>
2023-10-11 16:53:39 -0300

    Update detect_face_with_mask.py

--- detect_face_with_mask.py
+++ detect_face_with_mask.py
@@ -1,9 +1,10 @@
 import cv2
 import numpy as np
 
-import tensorflow as tf
+#importar tensorflow
 
-model = tf.keras.models.load_model('keras_model.h5')
+#criar variável model para receber o modelo treinado. 
+
 
 video = cv2.VideoCapture(0)
 
@@ -13,19 +14,18 @@ while True:
 
     # Altere o dado de entrada:
     # 1. Redimensione a imagem
-    img = cv2.resize(frame,(224,224))
+   
 
     
 
     # 2. Converta a imagem em um array Numpy e aumente a dimensão para o funcionamento correto das imagens treinadas
-    test_image = np.array(img, dtype=np.float32)
-    test_image = np.expand_dims(test_image, axis=0)
+   
     
     # 3. Normalize a imagem (ajuda no treinamento eficaz da máquina, especialmente em redes neurais) (rapidez no processamento de imagens)
-    normalised_image = test_image/255.0
+   
 
     # Preveja o resultado
-    prediction = model.predict(normalised_image)
+    
 
    
 
